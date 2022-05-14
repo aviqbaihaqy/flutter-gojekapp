616035c1bb952dcf47f55f29f327305ee589a86a
FlutLab.io <>
2022-05-14 02:38:40 +0000

    Autosave

--- lib/beranda/beranda_view.dart
+++ lib/beranda/beranda_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gojek/beranda/beranda_gojek_appbar.dart';
 
 class BerandaPage extends StatefulWidget {
   const BerandaPage({Key? key}) : super(key: key);
@@ -8,6 +9,9 @@ class BerandaPage extends StatefulWidget {
 class _BerandaPageState extends State<BerandaPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: new GojekAppbar(),
+      body: new Container(),
+    );
   }
 }

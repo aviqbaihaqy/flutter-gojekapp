f829462046c44c90500ce2819c766e0f4df0356f
FlutLab.io <>
2022-05-14 01:45:23 +0000

    Autosave

---
+++ lib/launcher/launcher_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LauncherPage extends StatefulWidget {
+  const LauncherPage({Key? key}) : super(key: key);
+  _LauncherPageState createState() => _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

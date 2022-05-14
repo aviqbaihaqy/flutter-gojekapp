388761bf5e64feb141a0fd4e996aafcbfba500a4
FlutLab.io <>
2022-05-14 01:58:52 +0000

    Autosave

--- lib/launcher/launcher_view.dart
+++ lib/launcher/launcher_view.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import '../landingpage/landingpage_view.dart';
 
 class LauncherPage extends StatefulWidget {
   const LauncherPage({Key? key}) : super(key: key);
@@ -6,6 +10,23 @@ class LauncherPage extends StatefulWidget {
 }
 
 class _LauncherPageState extends State<LauncherPage> {
+  @override
+  void initState() {
+    super.initState();
+    startLaunching();
+  }
+
+  startLaunching() async {
+    var duration = const Duration(seconds: 3);
+    return new Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        new MaterialPageRoute(builder: (_) {
+          return new LandingPage();
+        }),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
